34b16d22b4a402927b538cc32f8b4ae7967769be
unknown <dimsam74@gmail.com>
2025-06-30 03:47:12 +0300

    add libvlc for linux provider

--- StreamA.Desktop/CameraProvider.cs
+++ StreamA.Desktop/CameraProvider.cs
@@ -1,4 +1,5 @@
-﻿using OpenCvSharp;
+﻿using LibVLCSharp.Shared;
+using OpenCvSharp;
 using StreamA.Services;
 using System;
 using System.Net.Sockets;
@@ -7,17 +8,36 @@ using System.Threading;
 
 namespace StreamA.Desktop
 {
+    public interface IFrameSender
+    {
+        bool IsOpened { get; }
+        event Action<string>? StatusChanged; // Для UI-индикации
+
+        void Start();
+        void Stop();
+    }
+
     internal class CameraProvider : ICameraProvider, IStatusProvider
     {
         public bool IsWorked => _sender?.IsOpened == true;
         public event Action<string>? StatusChanged;
 
-        private OpenCVSharp4FrameSender? _sender;
+        private IFrameSender? _sender;
         private TcpClient? _client;
         private NetworkStream? _stream;
         private string _host = "127.0.0.1";
         private int _port = 12345;
         private Action<string>? _statusHandler;// Обработчик статуса для UI-индикации
+
+        private IFrameSender CreateCameraSender()
+        {
+            if (OperatingSystem.IsLinux())
+                return new LibVlcSharpFrameSender(SendFrame, "/dev/video0", 640, 480);//linux
+            if (OperatingSystem.IsWindows())
+                return new OpenCVSharp4FrameSender(SendFrame); //windous
+
+            throw new PlatformNotSupportedException();
+        }
 
         public void Start(string host, int port)
         {
@@ -28,11 +48,11 @@ namespace StreamA.Desktop
                 _client = new TcpClient(_host, _port);
                 _stream = _client.GetStream();
 
-                _sender = new OpenCVSharp4FrameSender(SendFrame);
+                _sender = CreateCameraSender();
                 _statusHandler = msg => StatusChanged?.Invoke(msg);
                 _sender.StatusChanged += _statusHandler;
             }
-            _sender.StartCapture();
+            _sender.Start();
         }
 
         public void Stop()
@@ -40,7 +60,7 @@ namespace StreamA.Desktop
             if (_sender != null)
             {
                 _sender.StatusChanged -= _statusHandler;
-                _sender.StopCapture();
+                _sender.Stop();
                 _sender = null;
                 _stream?.Dispose();
                 _client?.Close();
@@ -64,8 +84,82 @@ namespace StreamA.Desktop
             }
         }
 
-        #region -- incapsulate camera frame --
-        public class OpenCVSharp4FrameSender
+        #region -- incapsulate camera frame linux --
+        public class LibVlcSharpFrameSender : IFrameSender
+        {
+            public bool IsOpened => false;// _capture?.IsOpened() == true;
+            public event Action<string>? StatusChanged; // Для UI-индикации
+
+            private readonly string _devicePath;
+            private readonly uint _width, _height;
+            private MediaPlayer? _mediaPlayer;
+            private IntPtr _buffer = IntPtr.Zero;
+            private readonly Action<byte[]> _onFrame;
+
+            public LibVlcSharpFrameSender(Action<byte[]> onFrame, string devicePath, uint width, uint height)
+            {
+                Core.Initialize();
+
+                _onFrame = onFrame;
+                _devicePath = devicePath;
+                _width = width;
+                _height = height;
+            }
+
+            public void Start()
+            {
+                var libVlc = new LibVLC();
+                var media = new Media(libVlc, $"v4l2://{_devicePath}", FromType.FromLocation);
+                _mediaPlayer = new MediaPlayer(media);
+
+                _mediaPlayer.SetVideoFormat("RV32", _width, _height, _width * 4);
+                _mediaPlayer.SetVideoCallbacks(
+                    lockCb: (opaque, planes) =>
+                    {
+                        // Выделяем буфер под кадр
+                        if (_buffer == IntPtr.Zero)
+                            _buffer = Marshal.AllocHGlobal((int)(_width * _height * 4));
+                        //planes = _buffer;
+                        unsafe
+                        {
+                            var planePtr = (IntPtr*)planes;
+                            planePtr[0] = _buffer;
+                        }
+                        return IntPtr.Zero;
+                    },
+                    unlockCb: (opaque, picture, planes) =>
+                    {
+                        // Здесь можно обработать кадр (planes)
+                        int size = (int)(_width * _height * 4);
+                        byte[] buffer = new byte[size];
+                        //Marshal.Copy(planes, buffer, 0, size);
+                        unsafe
+                        {
+                            var planePtr = (IntPtr*)planes;
+                            planePtr[0] = _buffer;
+                            Marshal.Copy(planePtr[0], buffer, 0, size);
+                        }
+                        _onFrame(buffer); // передаём кадр в обработку
+                    },
+                    displayCb: (opaque, picture) => { }
+                );
+                _mediaPlayer.Play();
+            }
+
+            public void Stop()
+            {
+                _mediaPlayer?.Stop();
+                if (_buffer != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(_buffer);
+                    _buffer = IntPtr.Zero;
+                }
+            }
+        }
+        #endregion
+
+        #region -- incapsulate camera frame windows --
+        public class OpenCVSharp4FrameSender: IFrameSender
         {
             public bool IsOpened => _capture?.IsOpened() == true;
             public event Action<string>? StatusChanged; // Для UI-индикации
@@ -73,15 +167,17 @@ namespace StreamA.Desktop
             private VideoCapture? _capture;
             private Thread? _captureThread;
             private volatile bool _running;
-            private readonly Action<byte[]> _onFrame;
             private string _fourccStr = null!;
+            private readonly Action<byte[]> _onFrame;
+            private readonly int _deviceIndex;
 
-            public OpenCVSharp4FrameSender(Action<byte[]> onFrame)
+            public OpenCVSharp4FrameSender(Action<byte[]> onFrame, int deviceIndex = 0)
             {
                 _onFrame = onFrame;
+                _deviceIndex = deviceIndex;
             }
 
-            public void StartCapture(int deviceIndex = 0)
+            public void Start()
             {
                 if (_running || IsOpened)
                 {
@@ -90,7 +186,7 @@ namespace StreamA.Desktop
                 }
 
                 var backendApi = GetPreferredApi();
-                _capture = new VideoCapture(deviceIndex, backendApi);
+                _capture = new VideoCapture(_deviceIndex, backendApi);
                 if (_capture.CaptureType == CaptureType.Camera)
                 {
                     if (_capture.FrameWidth > 1920 || _capture.FrameHeight > 1080)
@@ -119,7 +215,7 @@ namespace StreamA.Desktop
                 StatusChanged?.Invoke($"Camera started => Backend API: {_capture?.GetBackendName()}; Opened with FourCC: {_fourccStr}; Resolution: {_capture?.FrameWidth}x{_capture?.FrameHeight}");
             }
 
-            public void StopCapture()
+            public void Stop()
             {
                 if (!_running && !IsOpened)
                     return;
@@ -133,7 +229,7 @@ namespace StreamA.Desktop
                 StatusChanged?.Invoke("Camera stopped");
             }
 
-            public static VideoCaptureAPIs GetPreferredApi()
+            private static VideoCaptureAPIs GetPreferredApi()
             {
                 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
                     return VideoCaptureAPIs.DSHOW; // или MSMF по ситуации
@@ -147,7 +243,7 @@ namespace StreamA.Desktop
                 return VideoCaptureAPIs.ANY;
             }
 
-            public void CaptureLoop()
+            private void CaptureLoop()
             {
                 var converter = FrameConverterFactory.GetConverter(_fourccStr);
                 using var mat = new Mat();
